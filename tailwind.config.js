d7b77ea9a96c0d058f66ddc08496c541d5d1cd4d
ergofriend <ergofriend@kasu.dev>
2024-08-12 15:39:58 +0900

    add rippleui

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,5 @@
+import rippleui from "rippleui"
+
 /** @type {import('tailwindcss').Config} */
 export default {
 	content: [
@@ -7,5 +9,5 @@ export default {
 	theme: {
 		extend: {},
 	},
-	plugins: [],
+	plugins: [rippleui],
 }
