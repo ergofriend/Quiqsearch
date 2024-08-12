c577f31fbc890512368db6ffb11b3e92996d29a5
ergofriend <ergofriend@kasu.dev>
2024-08-12 16:43:25 +0900

    TODOs

--- components/organisms/advanced-settings.tsx
+++ components/organisms/advanced-settings.tsx
@@ -1,1 +1,10 @@
-export const AdvancedSettings = () => {}
+export const AdvancedSettings = () => {
+	return (
+		<div>
+			{/* TODO: filter(allow/block) */}
+			{/* TODO: invalid targets(input, textarea, etc.) */}
+			{/* TODO: internal filters(youtube, twitter, etc.) */}
+			{/* TODO: theme(auto/dark/light) */}
+		</div>
+	)
+}
--- components/organisms/basic-settings.tsx
+++ components/organisms/basic-settings.tsx
@@ -3,7 +3,9 @@ import { ExtensionSwitch } from "@/components/molecules/extension-switch"
 export const BasicSettings = () => {
 	return (
 		<div>
+			{/* TODO: GitHub repo link */}
 			<ExtensionSwitch />
+			{/* TODO: mode(manual/auto) */}
 		</div>
 	)
 }
