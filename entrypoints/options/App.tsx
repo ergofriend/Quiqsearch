eaf972ef1093292631024b911bd7de612921269c
ergofriend <ergofriend@kasu.dev>
2024-08-12 16:51:27 +0900

    add テストページ（GitHub）

--- entrypoints/options/App.tsx
+++ entrypoints/options/App.tsx
@@ -5,6 +5,14 @@ function App() {
 	return (
 		<div className="flex flex-col gap-4">
 			<h1 className="text-3xl">{browser.i18n.getMessage("detail_settings")}</h1>
+
+			<a
+				className="link link-underline"
+				href="https://github.com/ergofriend/Quiqsearch"
+			>
+				テストページ（GitHub）
+			</a>
+
 			<BasicSettings />
 		</div>
 	)
