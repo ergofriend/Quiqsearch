9ca3343d86bff6939c1daaaa8f7419538b210b4b
ergofriend <ergofriend@kasu.dev>
2024-08-24 01:01:33 +0900

    search selection with Google

--- entrypoints/content.ts
+++ entrypoints/content.ts
@@ -1,16 +1,51 @@
 import { logger } from "@/libs/logger"
+import { debounce } from "es-toolkit"
 
-const handleSelectionChange = () => {
-	const selection = window.getSelection()?.toString()
-	if (!selection) return
-	logger.log("sendMessage:", selection)
-	browser.runtime.sendMessage(selection)
+const searchWithGoogle = (keyword: string) => {
+	const url = new URL("https://www.google.com/search")
+	url.searchParams.set("q", keyword)
+	window.open(url.toString(), "_blank")
+	console.info("searchWithGoogle:", keyword)
 }
 
 export default defineContentScript({
 	matches: ["<all_urls>"],
 	main() {
 		logger.info("Content Script loaded.")
-		document.addEventListener("selectionchange", handleSelectionChange)
+		let controller = new AbortController()
+
+		const init = (c: AbortController) => {
+			const debounceMs = 2000
+			const debouncedWithSignalFunction = debounce(
+				(text: string) => {
+					searchWithGoogle(text)
+				},
+				debounceMs,
+				{ signal: c.signal },
+			)
+
+			const handleSelectionChange = () => {
+				const selection = window.getSelection()?.toString()
+				logger.log("handleSelectionChange:", selection)
+				if (!selection) return
+				debouncedWithSignalFunction(selection)
+			}
+
+			console.log("handleSelectionChange registered.")
+			document.addEventListener("selectionchange", handleSelectionChange)
+		}
+
+		init(controller)
+
+		document.addEventListener("visibilitychange", () => {
+			if (document.visibilityState === "hidden") {
+				logger.info("Tab is hidden.")
+				controller.abort()
+			} else {
+				logger.info("Tab is visible.")
+				controller = new AbortController()
+				init(controller)
+			}
+		})
 	},
 })
