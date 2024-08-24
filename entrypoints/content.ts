1636f1d3841303a4ac12d06fb3b7654e5c15cb36
ergofriend <ergofriend@kasu.dev>
2024-08-24 13:02:21 +0900

    sync extension config

--- entrypoints/content.ts
+++ entrypoints/content.ts
@@ -1,4 +1,5 @@
 import { logger } from "@/libs/logger"
+import { extensionFeatureState } from "@/libs/storage"
 import { debounce } from "es-toolkit"
 
 const searchWithGoogle = (keyword: string) => {
@@ -8,44 +9,77 @@ const searchWithGoogle = (keyword: string) => {
 	console.info("searchWithGoogle:", keyword)
 }
 
-export default defineContentScript({
-	matches: ["<all_urls>"],
-	main() {
-		logger.info("Content Script loaded.")
-		let controller = new AbortController()
+const handleSelectionChange = (event: string, c: AbortController) => {
+	const debounceMs = 2000
+	const debouncedWithSignalFunction = debounce(
+		(text: string) => {
+			searchWithGoogle(text)
+		},
+		debounceMs,
+		{ signal: c.signal },
+	)
 
-		const init = (c: AbortController) => {
-			const debounceMs = 2000
-			const debouncedWithSignalFunction = debounce(
-				(text: string) => {
-					searchWithGoogle(text)
-				},
-				debounceMs,
-				{ signal: c.signal },
-			)
+	return () => {
+		const selection = window.getSelection()?.toString()
+		logger.debug(event, "handleSelectionChange:", selection)
+		if (!selection) return
+		debouncedWithSignalFunction(selection)
+	}
+}
 
-			const handleSelectionChange = () => {
-				const selection = window.getSelection()?.toString()
-				logger.log("handleSelectionChange:", selection)
-				if (!selection) return
-				debouncedWithSignalFunction(selection)
-			}
+const init = (event: string, c: AbortController) => {
+	console.log(event, "handleSelectionChange registered.")
+	document.removeEventListener(
+		"selectionchange",
+		handleSelectionChange(event, c),
+	)
+	document.addEventListener("selectionchange", handleSelectionChange(event, c))
+}
 
-			console.log("handleSelectionChange registered.")
-			document.addEventListener("selectionchange", handleSelectionChange)
-		}
+const main = () => {
+	logger.info("Content Script loaded.")
 
-		init(controller)
+	let controller = new AbortController()
+	const renewController = () => {
+		controller = new AbortController()
+	}
 
-		document.addEventListener("visibilitychange", () => {
-			if (document.visibilityState === "hidden") {
-				logger.info("Tab is hidden.")
+	const initExtension = () => {
+		extensionFeatureState.storage.getValue().then((isExtensionEnabled) => {
+			logger.log("initExtension.isExtensionEnabled:", isExtensionEnabled)
+			if (!isExtensionEnabled) return
+			renewController()
+			init("initExtension.storage.getValue", controller)
+		})
+	}
+
+	const syncExtension = () => {
+		extensionFeatureState.storage.watch((isExtensionEnabled) => {
+			logger.info("syncExtension.isExtensionEnabled:", isExtensionEnabled)
+			if (!isExtensionEnabled) {
 				controller.abort()
 			} else {
-				logger.info("Tab is visible.")
-				controller = new AbortController()
-				init(controller)
+				renewController()
+				init("syncExtension.watch", controller)
 			}
 		})
-	},
+	}
+
+	initExtension()
+	syncExtension()
+
+	document.addEventListener("visibilitychange", () => {
+		if (document.visibilityState === "hidden") {
+			logger.info("Tab is hidden.")
+			controller.abort()
+		} else {
+			logger.info("Tab is visible.")
+			initExtension()
+		}
+	})
+}
+
+export default defineContentScript({
+	matches: ["<all_urls>"],
+	main,
 })
