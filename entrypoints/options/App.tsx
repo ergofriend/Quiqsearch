9f9a6e77e3bdbefb6beef1a440f64fcab6fe3e5c
ergofriend <ergofriend@kasu.dev>
2024-09-16 20:57:37 +0900

    fix hero message

--- entrypoints/options/App.tsx
+++ entrypoints/options/App.tsx
@@ -13,10 +13,12 @@ function App() {
 
 				<h2
 					style={{ lineHeight: 1.5 }}
-					className="text-4xl md:text-4xl lg:text-6xl font-semibold max-w-7xl mx-auto text-center relative z-20 py-6 bg-clip-text text-transparent bg-gradient-to-b from-neutral-800 via-neutral-700 to-neutral-700 dark:from-neutral-800 dark:via-white dark:to-white"
+					className="text-4xl break-keep md:text-4xl lg:text-6xl font-semibold max-w-7xl mx-auto text-center relative z-20 py-6 bg-clip-text text-transparent bg-gradient-to-b from-neutral-800 via-neutral-700 to-neutral-700 dark:from-neutral-800 dark:via-white dark:to-white"
 				>
 					{browser.i18n.getMessage("option_description_1")}
-					<Cover>{browser.i18n.getMessage("option_description_2")}</Cover>
+					<span className="px-2">
+						<Cover>{browser.i18n.getMessage("option_description_2")}</Cover>
+					</span>
 					{browser.i18n.getMessage("option_description_3")}
 				</h2>
 
